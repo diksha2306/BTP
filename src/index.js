2f784764e8b89321cf3e45430f71654dc8d2b34e
diksha2306 <dikshak2003@gmail.com>
2024-12-03 20:35:44 +0530

    pushing the changes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from "@react-oauth/google"
-require('dotenv').config()
+// require('dotenv').config()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<GoogleOAuthProvider clientId={process.env.clientId}>
-	  <React.StrictMode>
-		<App />
-	  </React.StrictMode>
-	</GoogleOAuthProvider>
-  );
+	<BrowserRouter>
+		<GoogleOAuthProvider clientId="408125923641-j0o0rfjqj211fuga1nv58j5okc9cefet.apps.googleusercontent.com">
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</GoogleOAuthProvider>
+	</BrowserRouter>
+);
